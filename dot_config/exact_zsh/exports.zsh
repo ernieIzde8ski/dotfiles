cfd322defba8001c977f0a133ff0c49fd705d6b7
Ernest Izdebski <44341687+ernieIzde8ski@users.noreply.github.com>
2024-02-04 19:07:30 -0500

    exports.zsh: disable vscode as $EDITOR

--- dot_config/exact_zsh/exports.zsh
+++ dot_config/exact_zsh/exports.zsh
@@ -25,18 +25,10 @@ export CLICOLOR=1
 export LESSCHARSET="utf-8" # fix man pages not displaying apostrophes
 alias ls="ls --color=auto"
 
-# set vscode as $EDITOR when run in a vscode terminal
-if [[ "$VSCODE_SHELL_INTEGRATION" == 1 || "$VSCODE_INJECTION" == 1 ]]; then
-    # --wait makes it wait for files to close before exiting process
-    # - permits reading stdin
-    export EDITOR="$(command -v code) --wait -"
-# otherwise pick from micro, nano, or vim
-else
-    editors=("micro" "nano" "vim")
-    for editor in $editors; do
-        editor="$(command -v ${editor})" && {
-            export EDITOR=$editor
-            break
-        }
-    done
-fi
+editors=("micro" "nano" "vim")
+for editor in $editors; do
+    editor="$(command -v ${editor})" && {
+        export EDITOR=$editor
+        break
+    }
+done
