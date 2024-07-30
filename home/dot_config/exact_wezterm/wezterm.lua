1f250333da40beea389d88e607e3c8d27bbed06e
Ernest Izdebski <44341687+ernieIzde8ski@users.noreply.github.com>
2024-07-30 10:46:53 -0400

    wezterm: revise config options

--- home/dot_config/exact_wezterm/wezterm.lua
+++ home/dot_config/exact_wezterm/wezterm.lua
@@ -3,13 +3,20 @@ local wezterm = require("wezterm")
 local color_schemes = require("color_schemes")
 
 local config = {
+    clean_exit_codes = { 0, 130 },
+    exit_behavior = "CloseOnCleanExit",
+
     color_scheme = color_schemes.random(),
+
     font = wezterm.font_with_fallback({
         "CommitMonoErnie",
         "Comic Shanns Mono",
         "ComicShannsMonoNerdFont",
     }),
     font_size = 13.0,
+
+    prefer_to_spawn_tabs = true,
+    show_new_tab_button_in_tab_bar = false,
 }
 
 config.set_environment_variables = {
