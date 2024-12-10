e43cbee59064cc40957a418faa9b2fda96c16808
Ernest Izdebski <44341687+ernieIzde8ski@users.noreply.github.com>
2024-12-10 18:28:13 -0500

    feat(zsh): add cvim, dvim aliases
    - cvim: alias for `nvim --clean`
    - dvim: alias for `NVIM_APPNAME=nvim-devel nvim`, plus a whole buncha
      wacky logic

--- home/dot_config/exact_zsh/20_aliases.zsh
+++ home/dot_config/exact_zsh/20_aliases.zsh
@@ -21,7 +21,28 @@ alias info='info --vi-keys'
 alias sizeof="du -sh --"
 alias ...=../..
 alias prname=perl-rename
-alias ccr=cookiecutter
+
+alias cvim="nvim --clean"
+function dvim() (
+    set -e
+
+    dvim_parent="${XDG_CONFIG_HOME}"
+    dvim_caname='nvim-devel'
+    dvim_target="${dvim_parent}/${dvim_osname}"
+    dvim_source='https://github.com/ernieIzde8ski/dotfiles.nvim'
+
+    nvim_target="${XDG_CONFIG_HOME}/nvim"
+
+    if [[ ! -d "${dvim_target}" ]]; then
+        git -C "${dvim_parent}" clone "${dvim_source}" "${dvim_caname}"
+
+        if [[ -d "${nvim_target}" ]]; then
+            git -C "${nvim_target}" remote add "file://${dvim_target}"
+        fi
+    fi
+
+    NVIM_APPNAME="${dvim_caname}" nvim "$@"
+)
 
 
 if command -v zoxide &>/dev/null; then
